1234bf7094e868d81e2972b0e5db3f81a01a5218
Emanuel Orler <emanuel.orler@gmail.com>
2024-04-02 12:51:43 -0300

    Initial solution of the exercises

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
+app.get('/locations', (req, res) => {res.sendFile('locations.json', {root: __dirname})});
+app.get("/express", (req, res) => res.sendFile("index.html", { root: '\express' }));
+app.get("/cliente_servidor", (req, res) => res.sendFile("index.html", { root: '\cliente_servidor' }));
 app.use(express.static('public'))
+
 
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
